a84f6110849daa87bacf64670b68541b93e06d25
sakismaznis <sakismaz98@gmail.com>
2019-11-04 20:49:23 +0200

    Spring init

---
+++ src/main/java/com/WarNewsFeed/wnf/WnfApplication.java
@@ -0,0 +1,13 @@
+package com.WarNewsFeed.wnf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WnfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WnfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/WarNewsFeed/wnf/WnfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WarNewsFeed.wnf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WnfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

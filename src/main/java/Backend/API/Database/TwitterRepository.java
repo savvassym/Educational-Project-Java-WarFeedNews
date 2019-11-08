9997959ea45e3730735c29e1f7b41e12d0654bfb
Dimitris Vergetidis <dimitrisvergetidis@gmail.com>
2019-11-08 17:37:29 +0000

    Delete TwitterRepository.java

--- src/main/java/Backend/API/Database/TwitterRepository.java
+++
@@ -1,8 +0,0 @@
-package Backend.API.Database;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface TwitterRepository extends PagingAndSortingRepository<TwitterDB, Long> {
-
-        TwitterDB findByLocation(String location);
-}

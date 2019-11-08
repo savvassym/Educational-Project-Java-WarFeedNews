f43babaa0a31ad0433703cdd41d55e52f67d42e7
sakismaznis <sakismaz98@gmail.com>
2019-11-08 13:02:19 +0200

    Closed #4 Text analysis with Test done

--- src/main/java/com/WarNewsFeed/wnf/WnfApplication.java
+++ src/main/java/com/WarNewsFeed/wnf/WnfApplication.java
@@ -1,27 +1,36 @@
 package com.WarNewsFeed.wnf;
 
 import Backend.API.TwitterApi;
+import com.WarNewsFeed.wnf.NLP.Nlp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import twitter4j.Status;
 import twitter4j.TwitterException;
 
-import java.util.List;
+import java.sql.SQLException;
+import java.util.*;
 
 @SpringBootApplication
 public class WnfApplication {
 
-	public static void main(String[] args) throws TwitterException {
+	public static void main(String[] args) throws TwitterException, SQLException, ClassNotFoundException {
 		SpringApplication.run(WnfApplication.class, args);
-
 		List<Status> status;
 		TwitterApi twitterApi = new TwitterApi();
-
 		twitterApi.twitterConnection();
 		status = twitterApi.getData();
 		for(Status st : status) {
 			System.out.println(st.getUser().getName()+"========"+st.getText());
 		}
+
+		Nlp filter = new Nlp();
+		Map<String, String> collect;
+		collect = filter.analyzer("It takes a lifetime for someone to discover Greece, but it only takes an instance to fall in love with her.");
+		collect.forEach((key,value)-> System.out.println(key + " : "+ value));
+
+
+
+
 	}
 
 }

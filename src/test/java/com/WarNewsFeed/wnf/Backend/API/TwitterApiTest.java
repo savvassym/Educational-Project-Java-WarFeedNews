323c5631d3c9b5254a10c7f87cfd78374f5a7b79
Dimitris Vergetidis <dimitrisvergetidis@gmail.com>
2019-11-14 16:16:05 +0000

    Update TwitterApiTest.java

--- src/test/java/com/WarNewsFeed/wnf/Backend/API/TwitterApiTest.java
+++ src/test/java/com/WarNewsFeed/wnf/Backend/API/TwitterApiTest.java
@@ -1,24 +1,17 @@
 package com.WarNewsFeed.wnf.Backend.API;
 
+import org.junit.Test;
 import twitter4j.Status;
 import twitter4j.TwitterException;
-
 import java.util.List;
-
 import static org.junit.Assert.assertNotNull;
 
 public class TwitterApiTest {
 
-    @org.junit.Test
-    public void twitterConnection() throws TwitterException {
+    @Test
+    public void getTwitterPosts() throws TwitterException {
         TwitterApi connection = new TwitterApi();
-        connection.twitterConnection().getAccountSettings().equals("twitter4j.AccountSettingsJSONImpl@2a54a73f");
-    }
-
-    @org.junit.Test
-    public void getData() throws TwitterException {
-        TwitterApi connection = new TwitterApi();
-        List<Status> data = connection.getData();
+        List<Status> data = connection.getTwitterPosts();
         assertNotNull(data);
     }
 }
